e61eef875d3ee39935217c615537acb27872ba20
xunfeng911 <596927571@qq.com>
2017-08-15 14:58:50 +0800

    add router按需加载

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,31 +1,100 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Bundle from './bundle';
 
-// import App from '../components/App/App.js';
+// import HomeCon from 'bundle-loader?lazy&name=app-[name]!../components/home/home'
+// import AnnounceCon from 'bundle-loader?lazy&name=app-[name]!../components/announce/announce'
+// import MyProCon from 'bundle-loader?lazy&name=app-[name]!../components/mypro/mypro'
 
-// const RouteConfig = () => (
-//   <Router>
-//     <Switch>
-//       <Route path='/' exact component={App} />
-//     </Switch>
-//   </Router>
-// )
+// import UserCon from 'bundle-loader?lazy&name=app-[name]!../components/user/user'
+// import UsrInfoCon from 'bundle-loader?lazy&name=app-[name]!../components/user/usr_info'
+// import SetQQCon from 'bundle-loader?lazy&name=app-[name]!../components/user/set_qq'
+// import SetNameCon from 'bundle-loader?lazy&name=app-[name]!../components/user/set_name'
 
-// export default RouteConfig;
+// import LoginCon from 'bundle-loader?lazy&name=app-[name]!../components/login/login'
+// import RegisterCon from 'bundle-loader?lazy&name=app-[name]!../components/register/register'
+// import ResetCon from 'bundle-loader?lazy&name=app-[name]!../components/register/reset'
+// import NewPassCon from 'bundle-loader?lazy&name=app-[name]!../components/register/newpassword'
 
-import Home from '../components/home/home';
-import Announce from '../components/announce/announce';
-import MyPro from '../components/mypro/mypro';
 
-import User from '../components/user/user';
-import UsrInfo from '../components/user/usr_info';
-import SetQQ from '../components/user/set_qq';
-import SetName from '../components/user/set_name';
+const Home = (props) => (
+  <Bundle load={() => import('../components/home/home.jsx')}>
+    {(Home) => <Home {...props}/>}
+  </Bundle>
+)
 
-import Login from '../components/login/login';
-import Register from '../components/register/register';
-import Reset from '../components/register/reset';
-import NewPass from '../components/register/newpassword';
+const Announce = (props) => (
+  <Bundle load={() => import('../components/announce/announce.jsx')}>
+    {(Announce) => <Announce {...props}/>}
+  </Bundle>
+)
+
+const MyPro = (props) => (
+  <Bundle load={() => import('../components/mypro/mypro.jsx')}>
+    {(MyPro) => <MyPro {...props}/>}
+  </Bundle>
+)
+
+const User = (props) => (
+  <Bundle load={() => import('../components/user/user.jsx')}>
+    {(User) => <User {...props}/>}
+  </Bundle>
+)
+
+const UsrInfo = (props) => (
+  <Bundle load={() => import('../components/user/usr_info.jsx')}>
+    {(UsrInfo) => <UsrInfo {...props}/>}
+  </Bundle>
+)
+
+const SetQQ = (props) => (
+  <Bundle load={() => import('../components/user/set_qq.jsx')}>
+    {(SetQQ) => <SetQQ {...props}/>}
+  </Bundle>
+)
+
+const SetName = (props) => (
+  <Bundle load={() => import('../components/user/set_name.jsx')}>
+    {(SetName) => <SetName {...props}/>}
+  </Bundle>
+)
+
+const Login = (props) => (
+  <Bundle load={() => import('../components/login/login.jsx')}>
+    {(Login) => <Login {...props}/>}
+  </Bundle>
+)
+
+const Register = (props) => (
+  <Bundle load={() => import('../components/register/register.jsx')}>
+    {(Register) => <Register {...props}/>}
+  </Bundle>
+)
+
+const Reset = (props) => (
+  <Bundle load={() => import('../components/register/reset.jsx')}>
+    {(Reset) => <Reset {...props}/>}
+  </Bundle>
+)
+
+const NewPass = (props) => (
+  <Bundle load={() => import('../components/register/newpassword.jsx')}>
+    {(NewPass) => <NewPass {...props}/>}
+  </Bundle>
+)
+// import Home from '../components/home/home';
+// import Announce from '../components/announce/announce';
+// import MyPro from '../components/mypro/mypro';
+
+// import User from '../components/user/user';
+// import UsrInfo from '../components/user/usr_info';
+// import SetQQ from '../components/user/set_qq';
+// import SetName from '../components/user/set_name';
+
+// import Login from '../components/login/login';
+// import Register from '../components/register/register';
+// import Reset from '../components/register/reset';
+// import NewPass from '../components/register/newpassword';
 
 const RouteConfig = () => (
   <Router>
